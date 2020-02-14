2a3faa95c568f1cb6bbf847efb126a3443f43dd7
Helen Tunik <hgrishikashvili@plslogistics.com>
2020-02-14 13:40:55 +0200

    add solution for 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,13 @@
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const products = await Product.find(
+    { $text: { $search: ctx.query.query }},
+    { score: { $meta: 'textScore' }},
+  ).sort({ score: { $meta: 'textScore' }});
+  if (products.length > 0) {
+    ctx.body = { products };
+  } else {
+    ctx.body = { products: [] };
+  }
 };
